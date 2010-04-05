2d1601930a4ed1345eda263e1bed4774dac6220b
Zach Goldberg <zgold@corvette.(none)>
2010-04-05 01:25:19 -0400

    Add get_volume implementation

--- services/renderingcontrol.py
+++ services/renderingcontrol.py
@@ -1,4 +1,15 @@
+from client import get_client
 
-
-def get_volume():
+def get_volume(service, action):
+    MPDCLIENT = get_client()
     print "Get Volume"
+    
+    MPDCLIENT.connect()
+
+    status = MPDCLIENT.status()
+    action.set_value("ChannelVolume", str(status.get('volume', '0')))
+    MPDCLIENT.disconnect()
+
+    getattr(action, "return")()
+
+    
